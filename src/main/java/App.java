0e5028d5b6785e13a0c9a01a4b3a8d38c16bd137
User <izatert@gmail.com>
2021-03-16 22:08:14 +0300

    Commit_2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,11 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean == bean1);
+        System.out.println(cat == cat1);
     }
 }
